dcff54ab18037b31e21def5a8fdd684c82f515a0
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2021-07-08 15:46:38 +0200

    Latest update

--- arg.h
+++ arg.h
@@ -13,21 +13,21 @@ extern char *argv0;
 					argv[0] && argv[0][0] == '-'\
 					&& argv[0][1];\
 					argc--, argv++) {\
-				char argc_;\
-				char **argv_;\
-				int brk_;\
-				if (argv[0][1] == '-' && argv[0][2] == '\0') {\
-					argv++;\
-					argc--;\
-					break;\
-				}\
-				for (brk_ = 0, argv[0]++, argv_ = argv;\
-						argv[0][0] && !brk_;\
-						argv[0]++) {\
-					if (argv_ != argv)\
-						break;\
-					argc_ = argv[0][0];\
-					switch (argc_)
+				    char argc_;\
+				    char **argv_;\
+				    int brk_;\
+				    if (argv[0][1] == '-' && argv[0][2] == '\0') {\
+				    	argv++;\
+				    	argc--;\
+				    	break;\
+				    }\
+				    for (brk_ = 0, argv[0]++, argv_ = argv;\
+				    		argv[0][0] && !brk_;\
+				    		argv[0]++) {\
+				    	if (argv_ != argv)\
+				    		break;\
+				    	argc_ = argv[0][0];\
+				    	switch (argc_)
 
 #define ARGEND			}\
 			}
